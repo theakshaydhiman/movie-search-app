b3aa3d38edb6c954096b6af37c641989aef82452
Akshay Dhiman <akshay.virgo@gmail.com>
2019-01-06 18:09:50 +0530

    api endpoint http to https

--- src/actions/moviesActions.js
+++ src/actions/moviesActions.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import { GET_MOVIES } from './types';
 
-export const getMovies = (q) => async (dispatch) => {
-  const res = await axios.get(`http://www.omdbapi.com/?s=${q}&apikey=5ce2c41a&type=movie`);
+const getMovies = q => async (dispatch) => {
+  const res = await axios.get(`https://www.omdbapi.com/?s=${q}&apikey=5ce2c41a&type=movie`);
 
   dispatch({
     type: GET_MOVIES,
     payload: res.data.Search,
   });
 };
+
+export default getMovies;
